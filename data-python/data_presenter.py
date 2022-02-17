11eb96b15db539c8bf98a295c625d81bf3ca2733
Xeilon <simmojoh98@gmail.com>
2022-02-17 14:46:27 -0700

    completed basic lab

---
+++ data-python/data_presenter.py
@@ -0,0 +1,24 @@
+open_file = open('CupcakeInvoices.csv')
+
+# for row in open_file:
+#     print(row)
+
+# for row in open_file:
+#   item = row.split(',')
+#   print(item[2])
+
+# for row in open_file:
+#   items = row.split(',')
+#   total = int(items[3]) * float(items[4])
+#   print(total)
+
+# total = 0
+# for row in open_file:
+#   values = row.split(',')
+#   total = total + (int(values[3]) * float(values[4]))
+# print(total)
+
+
+
+
+open_file.close()
